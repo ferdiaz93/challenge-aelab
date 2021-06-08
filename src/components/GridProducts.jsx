1fcfff999a610931a415d0e0f85f45474a34db6f
ferdiaz93 <fernandodiaz1209@gmail.com>
2021-06-08 02:52:13 -0300

    pagination fixes

--- src/components/GridProducts.jsx
+++ src/components/GridProducts.jsx
@@ -1,11 +1,13 @@
 import Product from './Product'
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
-import { orderHighestPrice, orderLowestPrice } from '../utils'
+import { orderHighestPrice, orderLowestPrice, paginationProducts } from '../utils'
 
 const GridProducts = ({Points}) => {
     const [products, setProducts] = useState(null);
     const [activeFilter, setActiveFilter] = useState(null);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [parsedProducts, setParsedProducts] = useState([]);
 
     useEffect(() => {
         axios.get('https://coding-challenge-api.aerolab.co/products', {
@@ -15,6 +17,8 @@ const GridProducts = ({Points}) => {
         })
         .then(function (response) {
             setProducts(response.data);
+            let productsPages = paginationProducts(response.data);
+            setParsedProducts(productsPages);
         })
         .catch(function (error) {
             // handle error
@@ -33,20 +37,38 @@ const GridProducts = ({Points}) => {
     const orderLow = () => {
         const orderedProducts = orderLowestPrice(products);
         setProducts([...orderedProducts]);
-        setActiveFilter('Lower price')
+        updateProductsPages([...orderedProducts]);
+        setActiveFilter('Lower price');
     }
 
     const orderHigh = () => {
         const orderedProducts = orderHighestPrice(products);
-        setProducts([...orderedProducts])
+        setProducts([...orderedProducts]);
+        updateProductsPages([...orderedProducts]);
         setActiveFilter('Higher price')
+    }
+
+    const updateProductsPages = (productsOrdered) => {
+        let productsPages = paginationProducts(productsOrdered);
+        setParsedProducts(productsPages);
+        setCurrentPage(0);
+    }
+
+    const productsViewed = () => {
+        let count = 0;
+        let page = currentPage;
+        while(page >= 0){
+            count += parsedProducts[currentPage]?.length
+            page--
+        }
+        return count;
     }
 
     return (
         <>
             <section className="grid-products-filters">
                 <article className="quantity">
-                    <span>16 of 32 products</span>
+                    <span>{productsViewed()} of {products?.length} products</span>
                 </article>
                 <span className="grid-divider"></span>
                 <article className="sort-by">
@@ -57,9 +79,26 @@ const GridProducts = ({Points}) => {
                         <button className={`default-button ${activeFilter === 'Higher price' ? 'active' : 'none' }`} onClick={() => orderHigh()}>Highest price</button>
                     </div>
                 </article>
+                <article className="pages">
+                    <div className="container">
+                        {currentPage !== 0 ?  
+                            <img onClick={() => {setCurrentPage(currentPage - 1)}} src="./assets/icons/arrow-left.svg" alt="" />
+                        : null}
+                    </div>
+                    <div className="container">
+                        {currentPage !== parsedProducts.length - 1 ?  
+                            <img onClick={() => {setCurrentPage(currentPage + 1)}} src="./assets/icons/arrow-right.svg" alt="" />
+                        : null}
+                    </div>
+                </article>
             </section>
             <section className="grid-products-container">
-                {products ? products.map((product) => <Product Item={product} UserPoints={Points} key={product._id}></Product> ) : null}
+                {parsedProducts.length > 0 ? parsedProducts[currentPage].map((product) => <Product Item={product} UserPoints={Points} key={product._id}></Product> ) : null}
+            </section>
+            <section className="grid-products-filters footer">
+                <article className="quantity">
+                    <span>{productsViewed()} of {products?.length} products</span>
+                </article>
             </section>
         </>
     )
--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,12 @@ export const orderLowestPrice = (products) =>{
 export const orderHighestPrice = (products) =>{
     return products.sort((a, b) => b.cost - a.cost);
 }
+
+export const paginationProducts = ([...products]) => {
+    let newArray = []; 
+    while(products.length !== 0){
+        let page = products.splice(0, 16);
+        newArray.push(page);
+    }
+    return newArray;
+}
